9125bb720b8b73505c3ce0cce49fdbcd834ff337
Dara.Adekore <oluwadara.adekore@student.ufv.ca>
2021-03-24 17:10:00 -0700

    * Implemented Cloud storage for database storing     *Users should now be able to login on any device.     *Implemented using AWS S3     * This was not a trivial task haha... *Started the implementation of the ability to view a users Projects. This is the "Your projects" part of the app

---
+++ app/src/main/java/com/example/groupin/ProjectList.java
@@ -0,0 +1,96 @@
+package com.example.groupin;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.storage.options.StorageDownloadFileOptions;
+import com.amplifyframework.storage.options.StorageUploadFileOptions;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.text.DateFormatSymbols;
+import java.util.List;
+
+public class ProjectList extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ListView lvProjects;
+    String[] projects;
+    Button newProject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_list);
+
+        newProject = (Button) findViewById(R.id.newProjBut);
+        lvProjects = findViewById(R.id.lvProject);
+        projects = new DateFormatSymbols().getMonths();
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, projects);
+        lvProjects.setAdapter(monthAdapter);
+        lvProjects.setOnItemClickListener(this);
+
+
+        newProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(ProjectList.this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+
+                    //showImageChooser(ProjectList.this);
+                }
+            }
+        });
+    }
+    /*public void showImageChooser(Activity activity){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activity.startActivityForResult(galleryIntent,2);
+    }*/
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String project = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getApplicationContext(), "Clicked: " + project, Toast.LENGTH_SHORT).show();
+    }
+
+
+}

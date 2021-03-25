7933b1276ea43d8380340e303340cdd2c8e4bebe
Simran <simran.ufv@gmail.com>
2021-03-25 18:49:39 +0530

    Added spinner to the status selection

--- app/src/main/java/com/example/groupin/AddProject.java
+++ app/src/main/java/com/example/groupin/AddProject.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -13,18 +14,26 @@ public class AddProject extends AppCompatActivity {
     EditText p_name,p_start, p_due;
     Button add_project_btn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_project);
 
+        Spinner statusSpinner= findViewById(R.id.pstatus);
+        ArrayAdapter <String> myAdapter = new ArrayAdapter<>(AddProject.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.statusOpt));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        statusSpinner.setAdapter(myAdapter);
+
         p_name = findViewById(R.id.enter_name);
         p_start = findViewById(R.id.enter_pstart);
         p_due = findViewById(R.id.enter_pdue);
         add_project_btn = findViewById(R.id.add_p);
+        String pstatus = statusSpinner.getSelectedItem().toString();
+
         add_project_btn.setOnClickListener(view -> {
             DBHelper myDB = new DBHelper(AddProject.this);
-            myDB.addProject(p_name.getText().toString().trim(),p_start.getText().toString().trim(),p_due.getText().toString().trim(), null);
+            myDB.addProject(p_name.getText().toString().trim(),p_start.getText().toString().trim(),p_due.getText().toString().trim(), pstatus);
         });
     }
 }
